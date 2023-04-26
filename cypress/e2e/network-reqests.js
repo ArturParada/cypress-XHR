8d8bc37e707c8bc5f78c099b008491641ccaa498
Artur <paradaartur@gmail.com>
2023-04-26 18:53:41 +0200

    net-request

--- cypress/e2e/network-reqests.js
+++ cypress/e2e/network-reqests.js
@@ -8,9 +8,33 @@ describe('Nrtwor Requests', () => {
     it('Get request', () => {
         cy.intercept({
             method: "GET",
-            url: "**/comments/*",
-        }).as("getComment")
+            url: "**/comments/*"
+        },
+            {
+                body: {
+                    "postId": 1,
+                    "id": 1,
+                    "name": "testName",
+                    "email": "test@gmail.com",
+                    "body": "test message"
+                }
+
+            }
+        ).as("getComment")
         cy.get(".network-btn").click()
         cy.wait('@getComment').its("response.statusCode").should("eq", 200)
+    });
+    it('Post request', () => {
+        cy.intercept("POST", "**/comments").as("postComment")
+        cy.get(".btn-success").click()
+        cy.wait('@postComment').should(({ request, response }) => {
+            console.log(request);
+            expect(request.body).to.include("email")
+            console.log(response);
+            expect(response.body).to.have.property("name", "Using POST in cy.intercept()")
+            
+            expect(request.headers).to.have.property("content-type")
+            expect(request.headers).to.have.property("origin", "https://example.cypress.io")
+        })
     });
 });
